8b964f6d53264b3ff933d3a2418aeb9bebbef20a
steven hunt <st3v3n.hunt@gmail.com>
2017-03-13 19:37:37 +0000

    :metal: Add tests and linting

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 
-console.time('Merging data took');
+(function app() {
+  console.time('Merging data took');
 
-const pomiData = fs.readFileSync('./input/pomi.json')
-const gpData = fs.readFileSync('./input/gp-data.json');
+  const pomiData = fs.readFileSync('./input/pomi.json');
+  const gpData = fs.readFileSync('./input/gp-data.json');
 
-const gps = JSON.parse(gpData);
-const pomi = JSON.parse(pomiData);
+  const gps = JSON.parse(gpData);
+  const pomi = JSON.parse(pomiData);
 
-const merged = gps.map((gp) => {
-  const matchedPomi = pomi.find((item) => {
-    return item.GPPracticeCode === gp.odsCode;
+  const merged = gps.map((gp) => {
+    const matchedPomi = pomi.find(item => item.GPPracticeCode === gp.odsCode);
+    // eslint-disable-next-line no-param-reassign
+    gp.supplier = matchedPomi ? matchedPomi.Supplier : undefined;
+    return gp;
   });
-  gp.supplier = matchedPomi ? matchedPomi.Supplier : undefined;
-  return gp;
-});
 
-fs.writeFileSync('./data/gp-data-merged.json', JSON.stringify(merged), 'utf8');
+  fs.writeFileSync('./data/gp-data-merged.json', JSON.stringify(merged), 'utf8');
 
-console.timeEnd('Merging data took');
+  console.timeEnd('Merging data took');
+}());
