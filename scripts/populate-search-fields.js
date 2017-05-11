96e7f43cc9d3e3f33e47e984d1f4ec050a53738a
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-05-11 17:08:32 +0100

    :diamonds: Add cleansed name search field
    Moved processing out into separate file to allow code reuse and to make
    it easier to work with.

---
+++ scripts/populate-search-fields.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+/* Since mongo does not support ES6 we can not lint with our current eslint config */
+
+
+String.prototype.removePunctuation = function () {
+    return this.replace(/[^\w\s]/g,'');
+};
+
+String.prototype.removeStopWords = function () {
+    return this.replace(/(dr|doctor|drs)/ig,'');
+};
+
+String.prototype.removeDuplicates = function () {
+  return this
+    .split(' ')
+    .filter(function(item, pos, self) { return self.indexOf(item) == pos })
+    .join(' ');
+};
+
+const cleanse = function(searchFields) {
+  return searchFields 
+    .join(' ')
+    .toLowerCase()
+    .removePunctuation()
+    .removeStopWords()
+    .removeDuplicates()
+    .trim();
+}
+
+db.gps.find().snapshot()
+  .forEach(
+    function (gp) { 
+      gp.searchSurgery = cleanse(gp.address.addressLines.concat([gp.name, gp.address.postcode]));
+      gp.searchDoctors = cleanse(gp.doctors);
+      gp.searchName = cleanse([gp.name]);
+      db.gps.save(gp);
+    }
+  )
+
+db.gps.createIndex(
+  { 'searchName': 'text', 'searchSurgery': 'text', 'searchDoctors': 'text'},
+  { weights: {'searchName': 1, 'searchSurgery': 2, 'searchDoctors': 1}, 'default_language': 'none', 'name': 'SearchIndex'}
+)
